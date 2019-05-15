c75e2bcf6a144b7f7a77ef57bf46ac60e6102956
robinsongz <mrrobinsung@gmail.com>
2019-05-14 17:36:13 -0700

    changed favicon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faFacebook, faLinkedin, faGithub, faDiscord} from '@fortawesome/free-brands-svg-icons';
@@ -10,8 +9,3 @@ import { faInstagram, faFacebook, faLinkedin, faGithub, faDiscord} from '@fortaw
 library.add(faArrowDown, faInstagram, faFacebook, faLinkedin, faGithub, faDiscord)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
